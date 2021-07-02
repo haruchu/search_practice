9eda95dfe2b566da267b095912ce9e7c4cddfc37
haru <haruki6180@gmail.com>
2021-07-02 22:11:29 +0900

    検索機能できた

--- search/views.py
+++ search/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import People
 from django.views.generic import ListView
- 
- 
+
+
 class PeopleList(ListView):
-    model = People
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+
+        if query:
+            people_list = People.objects.filter(
+                name__icontains=query)
+        else:
+            people_list = People.objects.all()
+        return people_list
